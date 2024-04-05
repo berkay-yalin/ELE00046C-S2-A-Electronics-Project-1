e05d8ca2fcb947fede454bbf2d39efb30187096d
uckx <161267808+uckx@users.noreply.github.com>
2024-04-05 14:02:36 +0000

    meow

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "mbed.h"
-
+#include "STM32.hpp"
 // main() runs in its own thread in the OS
 int main()
 {
---
+++ src/pins.hpp
@@ -0,0 +1,20 @@
+#ifndef MY_STM32_LIBRARY_PINS_HPP
+#define MY_STM32_LIBRARY_PINS_HPP
+
+namespace STM32 {
+  constexpr uint8_t LCD_REG = PB1; // LCD register select pin
+    constexpr uint8_t LCD_EN = PB0; // LCD enable pin
+    constexpr uint8_t LCD_D4 = PA4; // LCD data pin 4
+    constexpr uint8_t LCD_D5 = PA5; // LCD data pin 5
+    constexpr uint8_t LCD_D6 = PA6; // LCD data pin 6
+    constexpr uint8_t LCD_D7 = PA7; // LCD data pin 7
+
+    constexpr uint8_t BUTTON_1 = PD0; // Button 1 pin
+    constexpr uint8_t BUTTON_2 = PD1; // Button 2 pin
+    constexpr uint8_t BUTTON_3 = PD2; // Button 3 pin
+    constexpr uint8_t BUTTON_4 = PD3; // Button 4 pin
+
+    constexpr uint8_t IR_REMOTE_RX = PE0; // IR remote receiver pin
+}
+
+#endif
