8399a5b1891d21b634de9366f1b84b1111358d1a
George <161267808+uckx@users.noreply.github.com>
2024-04-10 21:09:49 +0000

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include "mbed.h"
 #include "STM32.hpp"
-// main() runs in its own thread in the OS
+
 int main()
 {
     while (true) {
+ // bootup screen
+const int reg = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+
+lcd.begin(16, 2);
+
+lcd.clear();
+
+lcd.setCursor(0, 1);
+
+for (int i = 8; i >= 0; i--) {
+    lcd.print(" ");  
+lcd.setCursor(i, 1);
+    lcd.print("neutronics");
+    delay(200);
+  }
+}
+void loop() {
+ 
+}
 
     }
 }
